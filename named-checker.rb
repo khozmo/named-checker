5e803224e12e39dfe19de917fd14043ef72b28f4
Hayden Katzenellenbogen <hayden@nextlevelinternet.com>
2011-08-15 15:30:47 -0700

    all done for now

--- named-checker.rb
+++ named-checker.rb
@@ -1,3 +1,5 @@
+#!/usr/bin/env ruby
+
 require 'rubygems'
 require 'whois'
 require 'pp'
@@ -6,25 +8,44 @@ if ARGV[0].nil? then
   puts("please enter path to named.conf") 
   exit
 end
+
 count = 0
+output_file = File.new("named-output.csv", "w")
+
 File.open("#{ARGV[0]}").each {|line|
-  if line =~ /^zone/ && line !~ /arpa/ then
+  if line =~ /zone / && line !~ /arpa/ then
+    domain_name =  /zone\ "(.*)"/.match(line).to_a
+    puts count += 1
+    csv = []
     begin
-      a = Whois.whois("#{line.match(/^zone\ \"(.*)\"/)[1]}")
-    rescue Exception
+      a = Whois.whois("#{domain_name[1]}")
+    rescue Exception => e
+      puts e.message
+      pp domain_name[1]
+      pp line
+      csv << domain_name[1]+","+e.message
+      output_file.puts csv.join(",")
       next
     end
-    pp line.match(/^zone\ \"(.*)\"/)[1]
-    begin
-      a.nameservers.each do |ns|
-      puts ns
+    
+    csv << domain_name[1]
+    
+    if  a.registered? then
+      begin
+        a.nameservers.each {|ns| csv << ns[0]}
+      rescue
+        csv << "failed lookup"
+        output_file.puts csv.join(",")
+        next
+      end
+    else
+      csv << "no longer registered"
+      output_file.puts csv.join(",")
+      next
     end
-  rescue
-    puts "nameservers failed lookup"
-    next
-  end
-    sleep 10 if line.include?(".org") || line.include?(".gov") 
-  count += 1
-  pp count
+  
+    sleep 20 if line.include?(".org") || line.include?(".gov") 
+    output_file.puts csv.join(",")
   end
 }
+output_file.close
