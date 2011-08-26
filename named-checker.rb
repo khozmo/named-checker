53340f27395ce7634a4a19e7fa9b59619f51e138
Hayden Katzenellenbogen <hayden@nextlevelinternet.com>
2011-08-26 15:08:35 -0700

    sorted the nameservers before inserting into file.

--- named-checker.rb
+++ named-checker.rb
@@ -5,7 +5,7 @@ require 'whois'
 require 'pp'
 
 if ARGV[0].nil? then
-  puts("please enter path to named.conf") 
+  puts("usage: ./named-checker.rb [path to config file]") 
   exit
 end
 
@@ -13,7 +13,7 @@ count = 0
 output_file = File.new("#{ARGV[0].gsub('.','-')}.csv", "w")
 
 File.open("#{ARGV[0]}").each {|line|
-  if line =~ /zone / && line !~ /arpa/ then
+  if line =~ /^zone / && line !~ /arpa/ then
     domain_name =  /zone\ "(.*)"/.match(line).to_a
     puts count += 1
     csv = []
@@ -28,24 +28,27 @@ File.open("#{ARGV[0]}").each {|line|
       next
     end
     
-    csv << domain_name[1]
-    
+    #csv << domain_name[1]
+    servers =[]
     if  a.registered? then
       begin
-        a.nameservers.each {|ns| csv << ns[0]}
+        a.nameservers.each {|ns| servers << ns[0].downcase}
       rescue
-        csv << "failed lookup"
-        output_file.puts csv.join(",")
+        #csv << "failed lookup"
+        output_file.puts ("#{domain_name[1]}, failed lookup")
         next
       end
     else
-      csv << "no longer registered"
-      output_file.puts csv.join(",")
+      #csv << "no longer registered"
+      output_file.puts ("#{domain_name[1]}, no longer registered")
       next
     end
   
-    sleep 20 if line.include?(".org") || line.include?(".gov") 
-    output_file.puts csv.join(",")
+    sleep 20 if line.include?(".org") || line.include?(".gov")
+    puts "#{domain_name[1]} unsorted #{servers}"
+    servers.sort!
+    puts "#{domain_name[1]} sorted #{servers}"
+    output_file.puts ("#{domain_name[1]},#{servers.join(",")}")
   end
 }
 output_file.close
