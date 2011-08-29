2522c1331f8a6d20ec6e04fb0d554cb124ab8b49
Hayden Katzenellenbogen <hayden@nextlevelinternet.com>
2011-08-29 09:39:35 -0700

    working on zip attachment

--- named-checker.rb
+++ named-checker.rb
@@ -3,49 +3,54 @@
 require 'rubygems'
 require 'whois'
 require 'pp'
+require 'mail'
 
-if ARGV[0].nil? then
+if ARGV.empty? then
   puts("usage: ./named-checker.rb [path to config file]") 
   exit
 end
 
-count = 0
-output_file = File.new("#{ARGV[0].gsub('.','-')}.csv", "w")
+files_to_zip =[]
 
-File.open("#{ARGV[0]}").each {|line|
+ARGV.each { |zone_file|   
+count = 0
+output_file = File.new("#{zone_file.gsub('.','-')}.csv", "w")
+files_to_zip << output_file
+
+File.open(zone_file).each {|line|
   if line =~ /^zone / && line !~ /arpa/ then
-    domain_name =  /zone\ "(.*)"/.match(line).to_a
-    puts count += 1
+    domain_name =  line.match(/zone\ "(.*)"/)[1]
+#    puts domain_name
+#    puts count += 1
     csv = []
     begin
-      a = Whois.whois("#{domain_name[1]}")
+      a = Whois.whois(domain_name)
     rescue Exception => e
       puts e.message
-      pp domain_name[1]
+      pp domain_name
       pp line
-      csv << domain_name[1]+","+e.message
+      csv << domain_name+","+e.message
       output_file.puts csv.join(",")
       next
     end
     
-    #csv << domain_name[1]
     servers =[]
-    if  a.registered? then
+    if a.registered? then
       begin
         a.nameservers.each {|ns| servers << ns[0].downcase}
       rescue
-        #csv << "failed lookup"
-        output_file.puts ("#{domain_name[1]}, failed lookup")
+        output_file.puts ("#{domain_name}, failed lookup")
         next
       end
     else
-      #csv << "no longer registered"
-      output_file.puts ("#{domain_name[1]}, no longer registered")
+      output_file.puts ("#{domain_name}, no longer registered")
       next
     end
   
     sleep 20 if line.include?(".org") || line.include?(".gov")
-    output_file.puts ("#{domain_name[1]},#{servers.sort.join(",")}")
+    output_file.puts ("#{domain_name},#{servers.sort.join(",")}")
   end
 }
 output_file.close
+}
+
